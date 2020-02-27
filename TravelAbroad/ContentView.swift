1f00122d6f6089507f991a7036471411d62628a7
CharlesJV <charles764jaison@gmail.com>
2020-02-27 17:51:42 -0500

    Initial Commit

---
+++ TravelAbroad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravelAbroad
+//
+//  Created by Charles JV on 2020-02-27.
+//  Copyright © 2020 Charles JV. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
